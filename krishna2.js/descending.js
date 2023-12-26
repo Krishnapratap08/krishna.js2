a817ffb5a95eeccd091e034b9a63e4837c662027
Krishnapratap08 <109781485+Krishnapratap08@users.noreply.github.com>
2023-12-26 01:18:08 -0800

    Add files via upload

---
+++ krishna2.js/descending.js
@@ -0,0 +1,8 @@
+// Define an array of numbers (or strings) to sort in descending order
+const arr = [5, 3, 8, 1, 4, 9, 2, 7, 6];
+
+// Sort the array in descending order
+arr.sort((a, b) => b - a);
+
+// Print the sorted array
+console.log(arr); // Output will be: [9, 8, 7, 6, 5, 4, 3, 2, 1]
